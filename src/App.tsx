fdd6b4c5df701ee7c1dd54b776bf420f9afcbe1e
ShabnamBahramian <shabnambhm1891@gmail.com>
2021-11-06 20:37:24 +0330

    newsDetailPage

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-96 h-96 border border-black"> hi</div>
+    </div>
+  );
+}
+
+export default App;
